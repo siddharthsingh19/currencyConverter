3d61f0004b957c099fa4c997ae6fd575b379ad83
Siddharth Singh <siddharth.singh19@outlook.com>
2024-01-12 02:44:12 +0530

    Input fields and UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import InputBox from "./components/InputBox";
+
 function App() {
-  return <h1 className="bg-slate-500">Hola</h1>;
+  return (
+    <div className="h-screen w-screen bg-cover flex justify-center items-center" style={{backgroundImage:`url('https://w.wallhaven.cc/full/nr/wallhaven-nrll21.jpg')`}}>
+      <div className="m-10 bg-black/50 p-10 rounded-lg">
+        <h1 className="text-4xl text-center mb-2 text-white">Currency Converter</h1>
+      <InputBox />
+      </div>
+    </div>
+  );
 }
 
 export default App;
